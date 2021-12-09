10ab7db3e0aa0ed090aa4df9eed440a297616320
Dilmurod Allabergenov <mr.j0k3r.d1ma@gmail.com>
2021-12-09 14:47:20 +0500

    asdsd

--- modules/admin/views/default/sert.php
+++ modules/admin/views/default/sert.php
@@ -38,6 +38,24 @@
                 </a>
             </div>
 
+            <div class="col-md-3">
+                <a href="<?= Yii::$app->urlManager->createUrl(['/cp/sertificate-application'])?>">
+                    <div class="card">
+                        <!-- card body -->
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-10">
+                                    <h5 class="text-muted mb-3 lh-1 d-block text-truncate"><?= Yii::t('cp','Mahsulot ekspertizasi uchun arizalar')?></h5>
+                                </div>
+                                <div class="col-2">
+                                    <i class="fa fa-hotel"></i>
+                                </div>
+                            </div>
+                        </div><!-- end card body -->
+                    </div>
+                </a>
+            </div>
+
 
 
 
